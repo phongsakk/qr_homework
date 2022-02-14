406461f857bc52cab4bb4a3670a9a7c8ad3aecb6
Phongsak Yotsaodee <phongsak.ysd@gmail.com>
2022-02-14 19:38:36 +0700

    fix del.php

--- assign/del.php
+++ assign/del.php
@@ -2,5 +2,30 @@
 session_start();
 if (empty($_SESSION['UserID'])) header('Location:../login.php');
 if (empty($_GET['classid'])) header("Location:./");
+include("../sys_db_connect.php");
 
-echo ("Delete");
+$sql = "DELETE FROM tb_exercisesend WHERE ExerciseID IN (SELECT ExerciseID FROM tb_exercise WHERE ClassID=?)";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $_GET['classid']);
+$stmt->execute();
+$stmt->close();
+
+$sql = "DELETE FROM tb_exercise WHERE ClassID=?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $_GET['classid']);
+$stmt->execute();
+$stmt->close();
+
+$sql = "DELETE FROM tb_enroll WHERE ClassID=?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $_GET['classid']);
+$stmt->execute();
+$stmt->close();
+
+$sql = "DELETE FROM tb_class WHERE ClassID=?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $_GET['classid']);
+$stmt->execute();
+$stmt->close();
+
+echo "ลบสำเร็จ <a href='./'>กลับ</a>";
