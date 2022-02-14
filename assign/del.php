41e02f428d9f2486f07715815758fd784667978b
Phongsak Yotsaodee <phongsak.ysd@gmail.com>
2022-02-14 19:17:59 +0700

    delete

--- assign/del.php
+++ assign/del.php
@@ -2,6 +2,5 @@
 session_start();
 if (empty($_SESSION['UserID'])) header('Location:../login.php');
 if (empty($_GET['classid'])) header("Location:./");
-// include("../sys_db_connect.php");
-// $stmt = $mysqli->prepare("");
+
 echo ("Delete");
